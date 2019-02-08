de1c3524f0443ea8cadf599fab023d84b291b4eb
Valentin Podkopaev <max.astin@yandex.ru>
2019-02-08 21:55:31 +0300

    переделаны Messages, Message, MOE, MRE

--- app/src/main/java/com/example/ideal/myapplication/chat/Messages.java
+++ app/src/main/java/com/example/ideal/myapplication/chat/Messages.java
@@ -1,18 +1,415 @@
 package com.example.ideal.myapplication.chat;
 
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.LinearLayout;
 
 import com.example.ideal.myapplication.R;
+import com.example.ideal.myapplication.fragments.objects.Message;
+import com.example.ideal.myapplication.fragments.chatElements.MessageOrderElement;
+import com.example.ideal.myapplication.fragments.chatElements.MessageReviewElement;
+import com.example.ideal.myapplication.fragments.chatElements.notification_canceled_element;
+import com.example.ideal.myapplication.fragments.chatElements.notification_no_canceled_element;
+import com.example.ideal.myapplication.other.DBHelper;
 
 public class Messages extends AppCompatActivity {
 
+    private static final String TAG = "DBInf";
+
+    private static final String FILE_NAME = "Info";
+    private static final String PHONE_NUMBER = "Phone number";
+    private static final String DIALOG_ID = "dialog id";
+
+    private static final String SERVICE_ID = "service_id";
+
+    private String myPhone;
+    private String dialogId;
+    private String senderPhone;
+    private DBHelper dbHelper;
+
+    private FragmentManager manager;
+
+    LinearLayout messagesLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.messages);
 
+        dialogId = getIntent().getStringExtra(DIALOG_ID);
+        myPhone = getUserId();
+
+        manager = getSupportFragmentManager();
+        dbHelper = new DBHelper(this);
+
+        messagesLayout = findViewById(R.id.resultsMessageLayout);
+
+        // получаем телефон нашего собеседеника
+        senderPhone = getSenderPhone(dialogId);
     }
 
+    private String getSenderPhone(String dialogId) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        // Получает телефоны из диалога
+        // Таблицы: dialogs
+        // Условия: уточняем id диалога
+        String sqlQuery =
+                "SELECT "
+                        + DBHelper.KEY_FIRST_USER_ID_DIALOGS + ", "
+                        + DBHelper.KEY_SECOND_USER_ID_DIALOGS
+                        + " FROM "
+                        + DBHelper.TABLE_DIALOGS
+                        + " WHERE "
+                        + DBHelper.KEY_ID + " = ?";
+
+        Cursor cursor = database.rawQuery(sqlQuery, new String[]{dialogId});
+
+        if(cursor.moveToFirst()) {
+            int indexFirstPhone = cursor.getColumnIndex(DBHelper.KEY_FIRST_USER_ID_DIALOGS);
+            int indexSecondPhone = cursor.getColumnIndex(DBHelper.KEY_SECOND_USER_ID_DIALOGS);
+
+            String firstPhone = cursor.getString(indexFirstPhone);
+            String secondPhone = cursor.getString(indexSecondPhone);
+            cursor.close();
+            if(firstPhone.equals(myPhone)){
+                return secondPhone;
+            }
+            else {
+                return firstPhone;
+            }
+        }
+        cursor.close();
+        return "0";
+    }
+
+    private String getSenderName(String senderPhone) {
+
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        // Получает имя пользователя, который отправил нам сообщение
+        // Таблицы: Users
+        // Условия: уточняем id пользователя
+        String sqlQuery =
+                "SELECT "
+                        + DBHelper.KEY_NAME_USERS
+                        + " FROM "
+                        + DBHelper.TABLE_CONTACTS_USERS
+                        + " WHERE "
+                        + DBHelper.KEY_USER_ID + " = ?";
+        Cursor cursor = database.rawQuery(sqlQuery, new String[]{senderPhone});
+        if(cursor.moveToFirst()){
+            int indexName = cursor.getColumnIndex(DBHelper.KEY_NAME_USERS);
+            return cursor.getString(indexName);
+        }
+        cursor.close();
+        return "";
+    }
+
+    private void createMessages() {
+
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
+        Message message = new Message();
+        // Получает id сообщения, время сообщения
+        // Таблицы: messages
+        // Условия: уточняем id диалога
+        String messageQuery =
+                "SELECT "
+                        + DBHelper.KEY_ID + ", "
+                        + DBHelper.KEY_MESSAGE_TIME_MESSAGES
+                        + " FROM "
+                        + DBHelper.TABLE_MESSAGES
+                        + " WHERE "
+                        + DBHelper.KEY_DIALOG_ID_MESSAGES + " = ?"
+                        + " ORDER BY "
+                        + DBHelper.KEY_MESSAGE_TIME_MESSAGES;
+        Cursor messageCursor = database.rawQuery(messageQuery, new String[]{dialogId});
+
+        if (messageCursor.moveToFirst()) {
+            int indexMessageId = messageCursor.getColumnIndex(DBHelper.KEY_ID);
+            int indexMessageTime = messageCursor.getColumnIndex(DBHelper.KEY_MESSAGE_TIME_MESSAGES);
+
+            // Цикл по всем сообщениям в диалоге пользователя
+            do {
+                String messageTime = messageCursor.getString(indexMessageTime);
+                message.setMessageTime(messageTime);
+                message.setDialogId(dialogId);
+
+                // Проверяем какую информацию содержит сообщение (запись или ревью)
+                if(!isThisMessageContainsOrder(message)){
+                    String messageId = messageCursor.getString(indexMessageId);
+                    message.setId(messageId);
+
+                    isThisMessageContainsReview(message);
+                }
+            } while (messageCursor.moveToNext());
+            messageCursor.close();
+        }
+    }
+
+    private boolean isThisMessageContainsOrder(Message message) {
+
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
+        String orderQuery =
+                "SELECT "
+                        + DBHelper.KEY_ID + ", "
+                        + DBHelper.KEY_IS_CANCELED_ORDERS + ", "
+                        + DBHelper.KEY_WORKING_TIME_ID_ORDERS
+                        + " FROM "
+                        + DBHelper.TABLE_ORDERS
+                        + " WHERE "
+                        + DBHelper.KEY_MESSAGE_ID_ORDERS + " = ?";
+        Cursor orderCursor = database.rawQuery(orderQuery, new String[]{message.getId()});
+
+        if (orderCursor.moveToFirst()) {
+            boolean isCanceled = Boolean.valueOf(orderCursor.getString(orderCursor.getColumnIndex(DBHelper.KEY_IS_CANCELED_ORDERS)));
+            String timeId = orderCursor.getString(orderCursor.getColumnIndex(DBHelper.KEY_WORKING_TIME_ID_ORDERS));
+            String time = getTime(timeId);
+            String date = getDate(timeId);
+            String serviceId = getServiceId(timeId);
+            boolean isMyService = isMyService(serviceId);
+            String serviceName = getServiceName(serviceId);
+            String senderName = getSenderName(senderPhone);
+            String orderId = orderCursor.getString(orderCursor.getColumnIndex(DBHelper.KEY_ID));
+
+            message.setIsCanceled(isCanceled);
+            message.setIsMyService(isMyService);
+            message.setOrderTime(time);
+            message.setDate(date);
+            message.setServiceName(serviceName);
+            message.setUserName(senderName);
+            message.setTimeId(timeId);
+            message.setOrderId(orderId);
+
+            addMessageOrderToScreen(message);
+            orderCursor.close();
+            return true;
+        }
+        orderCursor.close();
+        return false;
+    }
+
+    private void isThisMessageContainsReview(Message message) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
+        String reviewQuery =
+                "SELECT "
+                        + DBHelper.KEY_RATING_REVIEWS + ", "
+                        + DBHelper.KEY_TYPE_REVIEWS + ", "
+                        + DBHelper.KEY_WORKING_TIME_ID_REVIEWS
+                        + " FROM "
+                        + DBHelper.TABLE_REVIEWS
+                        + " WHERE "
+                        + DBHelper.KEY_MESSAGE_ID_REVIEWS + " = ?";
+        Cursor reviewCursor = database.rawQuery(reviewQuery, new String[]{message.getId()});
+
+        if (reviewCursor.moveToFirst()) {
+            //int indexReview = reviewCursor.getColumnIndex(DBHelper.KEY_WORKING_TIME_ID_REVIEWS);
+            int indexRating = reviewCursor.getColumnIndex(DBHelper.KEY_WORKING_TIME_ID_REVIEWS);
+            int indexType = reviewCursor.getColumnIndex(DBHelper.KEY_WORKING_TIME_ID_REVIEWS);
+            int indexTimeId = reviewCursor.getColumnIndex(DBHelper.KEY_WORKING_TIME_ID_REVIEWS);
+
+            do{
+                boolean isRate = reviewCursor.getString(indexRating).equals("-");
+                String type = reviewCursor.getString(indexType);
+                String timeId = reviewCursor.getString(indexTimeId);
+
+                boolean isCanceled = getIsCanceled(timeId);
+                String time = getTime(timeId);
+                String date = getDate(timeId);
+                String serviceId = getServiceId(timeId);
+                //boolean isMyService = isMyService(serviceId);
+                String serviceName = getServiceName(serviceId);
+                String senderName = getSenderName(senderPhone);
+
+                //message.setIsMyService(isMyService);
+                message.setIsCanceled(isCanceled);
+                message.setIsRate(isRate);
+                message.setType(type);
+                message.setOrderTime(time);
+                message.setDate(date);
+                message.setServiceName(serviceName);
+                message.setUserName(senderName);
+                //message.setServiceId(serviceId);
+
+                addMessageReviewToScreen(message);
+            } while (reviewCursor.moveToNext());
+        }
+
+        reviewCursor.close();
+    }
+
+    private String getDate(String timeId) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        // Получает id сервиса
+        // Таблицы:  working time, working days, services
+        // Условия: уточняем id времени и id сервиса, объеденяем таблицы по id дня и id сервиса
+        String sqlQuery =
+                "SELECT "
+                        + DBHelper.KEY_DATE_WORKING_DAYS
+                        + " FROM "
+                        + DBHelper.TABLE_WORKING_DAYS
+                        + DBHelper.TABLE_WORKING_TIME
+                        + " WHERE "
+                        + DBHelper.TABLE_WORKING_TIME + "." + DBHelper.KEY_ID + " = ? "
+                        + " AND "
+                        + DBHelper.KEY_WORKING_DAYS_ID_WORKING_TIME
+                        + " = "
+                        + DBHelper.TABLE_WORKING_DAYS + "." + DBHelper.KEY_ID;
+
+        Cursor cursor = database.rawQuery(sqlQuery, new String[]{timeId});
+        String date = cursor.getString(cursor.getColumnIndex(DBHelper.KEY_DATE_WORKING_DAYS));
+
+        return date;
+    }
+
+    private String getTime(String timeId) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        // Получает время
+        // Таблицы:  working time
+        // Условия: уточняем id времени
+        String sqlQuery =
+                "SELECT "
+                        + DBHelper.KEY_TIME_WORKING_TIME
+                        + " FROM "
+                        + DBHelper.TABLE_WORKING_TIME
+                        + " WHERE "
+                        + DBHelper.KEY_ID + " = ? ";
+
+        Cursor cursor = database.rawQuery(sqlQuery, new String[]{timeId});
+        String time = cursor.getString(cursor.getColumnIndex(DBHelper.KEY_TIME_WORKING_TIME));
+
+        return time;
+    }
+
+    private String getServiceId(String timeId) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        // Получает id сервиса
+        // Таблицы:  working time, working days, services
+        // Условия: уточняем id времени и id сервиса, объеденяем таблицы по id дня и id сервиса
+        String sqlQuery =
+                "SELECT "
+                        + DBHelper.TABLE_CONTACTS_SERVICES + "." + DBHelper.KEY_ID
+                        + " AS " + SERVICE_ID
+                        + " FROM "
+                        + DBHelper.TABLE_CONTACTS_SERVICES
+                        + DBHelper.TABLE_WORKING_DAYS
+                        + DBHelper.TABLE_WORKING_TIME
+                        + " WHERE "
+                        + DBHelper.TABLE_WORKING_TIME + "." + DBHelper.KEY_ID + " = ? "
+                        + " AND "
+                        + DBHelper.KEY_WORKING_DAYS_ID_WORKING_TIME
+                        + " = "
+                        + DBHelper.TABLE_WORKING_DAYS + "." + DBHelper.KEY_ID
+                        + " AND "
+                        + DBHelper.TABLE_WORKING_DAYS + "." + DBHelper.KEY_SERVICE_ID_WORKING_DAYS
+                        + " = "
+                        + DBHelper.TABLE_CONTACTS_SERVICES + "." + DBHelper.KEY_ID;
+
+        Cursor cursor = database.rawQuery(sqlQuery, new String[]{timeId, myPhone});
+        String serviceId = cursor.getString(cursor.getColumnIndex(SERVICE_ID));
+
+        return serviceId;
+    }
+
+    private boolean getIsCanceled(String timeId) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        // Получает телефон юзера
+        // Таблицы:  working time
+        // Условия: уточняем id времени
+        String sqlQuery =
+                "SELECT "
+                        + DBHelper.KEY_USER_ID
+                        + " FROM "
+                        + DBHelper.TABLE_WORKING_TIME
+                        + " WHERE "
+                        + DBHelper.KEY_ID + " = ? ";
+
+        Cursor cursor = database.rawQuery(sqlQuery, new String[]{timeId});
+        String phone = cursor.getString(cursor.getColumnIndex(DBHelper.KEY_USER_ID));
+
+        return phone.equals("0");
+    }
+
+    // Пo id working time узнаёт ренадлежит ли мне скевис
+    private boolean isMyService(String serviceId) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        // Получает id сервиса
+        // Таблицы: services
+        // Условия: уточняем телефон пользователя и id сервиса
+        String sqlQuery =
+                "SELECT "
+                        + DBHelper.KEY_ID
+                        + " FROM "
+                        + DBHelper.TABLE_CONTACTS_SERVICES
+                        + " WHERE "
+                        + DBHelper.KEY_ID + " = ? "
+                        + " AND "
+                        + DBHelper.KEY_USER_ID + " = ?";
+
+        Cursor cursor = database.rawQuery(sqlQuery, new String[]{serviceId, myPhone});
+        return cursor.moveToFirst();
+    }
+
+    private String getServiceName(String serviceId) {
+
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        // Получает имя сервиса
+        // Таблицы: services
+        // Условия: уточняем id сервиса
+        String sqlQuery =
+                "SELECT "
+                        + DBHelper.KEY_NAME_SERVICES
+                        + " FROM "
+                        + DBHelper.TABLE_CONTACTS_SERVICES
+                        + " WHERE "
+                        + DBHelper.KEY_ID + " = ?";
+
+        Cursor cursor = database.rawQuery(sqlQuery, new String[]{serviceId});
+
+        if(cursor.moveToFirst()){
+
+            int indexName = cursor.getColumnIndex(DBHelper.KEY_NAME_SERVICES);
+
+            return cursor.getString(indexName);
+        }
+        cursor.close();
+        return "";
+    }
+
+    private void addMessageOrderToScreen(Message message) {
+        MessageOrderElement fElement = new MessageOrderElement(message);
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.resultsMessageLayout, fElement);
+        transaction.commit();
+    }
+
+    private void addMessageReviewToScreen(Message message) {
+        MessageReviewElement fElement = new MessageReviewElement(message);
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.resultsMessageLayout, fElement);
+        transaction.commit();
+    }
+
+    private String getUserId(){
+        SharedPreferences sPref = getSharedPreferences(FILE_NAME, MODE_PRIVATE);
+
+        return sPref.getString(PHONE_NUMBER, "-");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        messagesLayout.removeAllViews();
+        createMessages();
+    }
 }
