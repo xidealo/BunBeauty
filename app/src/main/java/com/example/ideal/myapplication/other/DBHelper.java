c8639d977734db1f959a1e13a8fb4be1d56b31c8
Valentin Podkopaev <max.astin@yandex.ru>
2019-02-11 20:52:38 +0300

    исправлена кнопка на MOE

--- app/src/main/java/com/example/ideal/myapplication/other/DBHelper.java
+++ app/src/main/java/com/example/ideal/myapplication/other/DBHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final  int DATABASE_VERSION = 66;
+    public static final  int DATABASE_VERSION = 67;
     public static final String DATABASE_NAME = "MyFirstDB";
 
     //tables name
