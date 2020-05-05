ab001b739adced283c49b2f023e9bedc2bacdd4b
xIdealo <shavl.mark@yandex.ru>
2020-05-05 14:19:21 +0300

    start subs on profile

---
+++ app/src/main/java/com/bunbeauty/ideal/myapplication/cleanArchitecture/business/profile/ProfileSubscriptionInteractor.kt
@@ -0,0 +1,12 @@
+package com.bunbeauty.ideal.myapplication.cleanArchitecture.business.profile
+
+import com.bunbeauty.ideal.myapplication.cleanArchitecture.business.profile.iProfile.IProfileSubscriptionInteractor
+import com.bunbeauty.ideal.myapplication.cleanArchitecture.data.db.models.entity.Subscription
+
+class ProfileSubscriptionInteractor : IProfileSubscriptionInteractor {
+
+    override fun addSubscription(subscription: Subscription) {
+
+    }
+
+}
--- app/src/main/java/com/bunbeauty/ideal/myapplication/cleanArchitecture/mvp/presenters/SubscriptionSubscriptionPresenter.kt
+++ app/src/main/java/com/bunbeauty/ideal/myapplication/cleanArchitecture/mvp/presenters/SubscriptionSubscriptionPresenter.kt
@@ -23,5 +23,4 @@ class SubscriptionSubscriptionPresenter(private val subscriptionInteractor: ISub
 
     }
 
-
 }
